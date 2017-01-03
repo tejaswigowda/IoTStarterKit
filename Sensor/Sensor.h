9eec94353bc9173e672a83d08c2a3f669104ab80
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-03 13:39:56 -0700

    Update Sensor.h

--- Sensor/Sensor.h
+++ Sensor/Sensor.h
@@ -6,15 +6,15 @@
 
 class Sensor{
 protected:
-	  		int objectID;
+	  		int   objectID;
 
 public:
-		    	Sensor(		  void);
-			int getObjectID(  void); //This does not need to be overridden
+		    	      Sensor(	  void);
+			int   getObjectID(  void); //This does not need to be overridden
 
-	//These functions must be overridden
-	virtual int   setupSensor(	void) = 0;
-	virtual float readSensor(	void) = 0;
+		//These functions must be overridden
+		virtual	int   setupSensor(	void) = 0;
+		virtual float readSensor(	void) = 0;
 };
 
 #endif
