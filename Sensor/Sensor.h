d512744c3c6a5afb667d4d4b35fab231f52b19d6
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-03 13:20:58 -0700

    Update Sensor.h

--- Sensor/Sensor.h
+++ Sensor/Sensor.h
@@ -14,8 +14,8 @@ public:
 		int getObjectID(	void); //This does not need to be overridden
 
 	//These functions must be overridden
-	virtual int setupSensor(	void) = 0;
-	virtual int readSensor(		void) = 0;
+	virtual int   setupSensor(	void) = 0;
+	virtual float readSensor(	void) = 0;
 };
 
 #endif
