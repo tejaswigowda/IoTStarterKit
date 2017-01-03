a0a5cedb55bad6cc5731920bdf1a0caf6e4d3ed5
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-03 12:19:18 -0700

    Update PushButton.h

--- PushButton/PushButton.h
+++ PushButton/PushButton.h
@@ -5,16 +5,17 @@
 
 class PushButton : public Sensor{
 protected:
-	const 	int pushButtonObjectID =  1;
-		int readPin 	       = -1; //-1 by default to force user to specify the readPin
+	const 	int pushButtonObjectID = 1;
+		int readPin           = -1; //-1 by default to force user to specify the readPin
 
 public:
 		    PushButton(	void);
-	            PushButton(	 int);
+		    PushButton(	 int);
 		int setReadPin(  int);
 
 		//inherited from Sensor.h
 		int setupSensor(void);
+		int setupSensor( int);
 		int readSensor( void);
 };
 
