4ef67417db2dba6af927a6b99d16e4fe2573fa43
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-03 10:58:40 -0700

    Create PushButton.h

---
+++ PushButton/PushButton.h
@@ -0,0 +1,21 @@
+#if !defined(PUSH_BUTTON_H)
+#define PUSH_BUTTON_H
+
+#include "Sensor.h"
+
+class PushButton : public Sensor{
+protected:
+	const 	int pushButtonObjectID = 1;
+			int readPin 		  = -1; //-1 by default to force user to specify the readPin
+
+public:
+				PushButton(	void);
+				PushButton(	 int);
+			int setReadPin(  int);
+
+			//inherited from Sensor.h
+			int setupSensor(void);
+			int readSensor( void);
+};
+
+#endif
