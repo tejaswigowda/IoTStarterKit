427d551decf5ba3fe95d6ff1c968e47c4a7ccc10
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-03 10:57:24 -0700

    Create Sensor.h

---
+++ Sensor/Sensor.h
@@ -0,0 +1,21 @@
+#if !defined(SENSOR_H)
+#define SENSOR_H
+
+#include "Peripheral.h"
+#include <Arduino.h>
+
+class Sensor{
+protected:
+	const 	int sensorObjectID = 0;
+	  		int objectID;
+
+public:
+				Sensor(				);
+			int getObjectID(	void); //This does not need to be overridden
+
+	//These functions must be overridden
+	virtual int setupSensor(	void) = 0;
+	virtual int readSensor(		void) = 0;
+};
+
+#endif
