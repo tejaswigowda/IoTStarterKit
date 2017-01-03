0ebbfaffd0184a206de2333618c08e7263cefdb9
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-03 10:58:55 -0700

    Add files via upload

---
+++ PushButton/PushButton.cpp
@@ -0,0 +1,43 @@
+#include "PushButton.h"
+
+
+PushButton::PushButton(){
+	objectID = pushButtonObjectID;
+}
+
+PushButton::PushButton(int newReadPin){
+	objectID = pushButtonObjectID;
+	if(newReadPin >= 0)
+		readPin = newReadPin;
+}
+
+int PushButton::setReadPin(int newReadPin){
+	if(newReadPin < 0){
+		return FAIL;
+	}
+
+	readPin = newReadPin;
+
+	return SUCCESS;
+}
+
+
+int PushButton::setupSensor(){
+	if(readPin >= 0){
+		return SUCCESS;
+	}
+	
+	return FAIL;
+}
+
+int PushButton::readSensor(){
+	if(readPin < 0){
+		return FAIL;
+	}
+
+	pinMode(readPin, INPUT);
+	digitalWrite(readPin, LOW);
+
+	return digitalRead(readPin);
+
+}
