aa1e1169dd8c2d8ea8b886f6579865f5ed1c3b8b
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-06 08:46:51 -0700

    Add files via upload

---
+++ lib/IoTDHT11/IoTDHT11.cpp
@@ -0,0 +1,63 @@
+#include "IoTDHT11.h"
+
+IoTDHT11::IoTDHT11(){
+	objectID = DHT11_ID;
+
+	sensor = new DHT(readPin, DHT11);
+}
+
+IoTDHT11::IoTDHT11(int newReadPin){
+	objectID = DHT11_ID;
+
+	if(newReadPin >= 0)
+		readPin = newReadPin;
+
+	sensor = new DHT(readPin, DHT11);
+}
+
+int IoTDHT11::setReadPin(int newReadPin){
+	if(newReadPin < 0){
+		return FAIL;
+	}
+
+	readPin = newReadPin;
+
+	return SUCCESS;
+}
+
+
+int IoTDHT11::setupSensor(){
+	if(readPin >= 0){
+		sensor->begin();
+		return SUCCESS;
+	}
+	
+	return FAIL;
+}
+
+int IoTDHT11::setupSensor(int newReadPin){
+	if(newReadPin >= 0){
+		readPin = newReadPin;
+		IoTDHT11::setupSensor();
+
+		return SUCCESS;
+	}
+
+	return FAIL;
+}
+
+float IoTDHT11::readSensor(){
+	if(readPin < 0){
+		return FAIL;
+	}
+
+	return sensor->readTemperature(false);
+}
+
+float IoTDHT11::readSensor(bool val){
+	if(val == HUMIDITY){
+		return sensor->readHumidity();
+	}
+	
+	return sensor->readTemperature(false);
+}
