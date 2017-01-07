32fe65c77c9dfe2658a32ab58125b8df9ca0b9c7
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-07 10:10:04 -0700

    Add files via upload

--- lib/Peripheral/Peripheral.h
+++ lib/Peripheral/Peripheral.h
@@ -1,11 +1,18 @@
+/*
+Phillip Noel
+05-January-2017
+*/
+
+
 #if !defined(PERIPHERAL_H)
 #define PERIPHERAL_H
 
-#define SUCCESS 0
-#define FAIL -1
+#define SUCCESS  0
+#define FAILURE -1
 #define UNKNOWN -1
 
 #define SENSOR_ID 0
 #define PUSHBUTTON_ID 1
+#define DHT11_ID 2
 
 #endif
