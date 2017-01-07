1c2e0cadd8e7fed11611f68a7f32d70ada860fec
PhillipNoel <phillip@foxyninjastudios.com>
2017-01-07 10:08:42 -0700

    Add files via upload

--- lib/Sensor/Sensor.h
+++ lib/Sensor/Sensor.h
@@ -1,20 +1,35 @@
+/*
+Phillip Noel
+05-January-2017
+*/
+
+
 #if !defined(SENSOR_H)
 #define SENSOR_H
 
 #include "Peripheral.h"
-#include <Arduino.h>
+
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+
+
 
 class Sensor{
 protected:
-	  		int   objectID;
+	  		int objectID;
 
 public:
-		    	      Sensor(	  void);
+		    	  Sensor(		void);
 			int   getObjectID(  void); //This does not need to be overridden
 
-		//These functions must be overridden
-		virtual	int   setupSensor(	void) = 0;
-		virtual float readSensor(	void) = 0;
+	//These functions must be overridden
+	virtual int   setupSensor(	void) = 0;
+	virtual float readSensor(	void) = 0;
 };
 
 #endif
