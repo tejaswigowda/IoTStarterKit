3bc25928f038513cce4b265446a9bb4fe098d640
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-01-10 10:59:46 -0700

    ac

--- IoTExperiments/SendMessage/server.js
+++ IoTExperiments/SendMessage/server.js
@@ -1,3 +1,19 @@
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtps://test%40foxyninjastudios.com:testuser@smtp.gmail.com');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"" <test@foxyninjastudios.com>', // sender address
+    to: 'tejaswil@gmail.com, 6238065387@txt.att.net', // list of receivers
+    subject: 'Test message', // Subject line
+    text: 'Hello world ?', // plaintext body
+    html: '<b>Hello world ?</b>' // html body
+};
+
+
+
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -12,8 +28,19 @@ app.get('/sendData', function(req, res){
     var h = info.h;
     var t = info.t;
     var key = info.key;
-//15974c1d771020e5
+    if( key !="15974c1d771020e5"){
+        res.send("noauth");
+        return;
+    }  
     console.log(h,t,key);
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+    });
+
 });
 app.get('/getState', function(req, res){
     res.writeHead(200, {"Content-Type": "text/plain"});
