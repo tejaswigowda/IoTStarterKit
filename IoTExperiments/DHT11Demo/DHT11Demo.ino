6619ed156db98c7055cd5bf13db053405ec9ef1c
PhillipNoel <Phillip.N.Noel@gmail.com>
2017-02-08 09:32:10 -0700

    Added Comments

--- IoTExperiments/DHT11Demo/DHT11Demo.ino
+++ IoTExperiments/DHT11Demo/DHT11Demo.ino
@@ -4,13 +4,13 @@
 
 #include "IoTDHT11.h"
 
-IoTDHT11 dht(8);
+IoTDHT11 dht(8);                                  //Create the object
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   
-  dht.setupSensor();
+  dht.setupSensor();                              //Setup the objects
   
   Serial.print("IoTDHT11 object id: ");
   Serial.println(dht.getObjectID());
@@ -19,7 +19,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Temperature (Celsius): \t");
+  Serial.print("Temperature (Celsius): \t");       //Read from the objects
   Serial.println(dht.readSensor(TEMPERATURE));
   
   Serial.print("Humidity (% humidity): \t");
