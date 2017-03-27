b64a7ab4c98a3ee2ade0214b222eee53018eacdd
phillip noel <phillip noel>
2017-03-27 12:12:02 -0700

    renamed a function. Demo sketch now up to date.

--- IoTExperiments/MCP3008/MCP3008Demo/MCP3008Demo.ino
+++ IoTExperiments/MCP3008/MCP3008Demo/MCP3008Demo.ino
@@ -11,7 +11,7 @@ void setup() {
 
   unsigned long tick = micros();
   for(int i = 0; i < 65000; i++){
-    readMCP(0, CSPIN);
+    readMCP3008(0, CSPIN);
   }
   unsigned long tock = micros();
 
@@ -21,7 +21,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(readMCP(0, CSPIN));
+  Serial.println(readMCP3008(0, CSPIN));
   delay(100);
 
 }
