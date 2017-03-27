52f8bda4962f5dd4a6bc4f36517c5ac25619aab2
phillip noel <phillip noel>
2017-03-27 12:11:05 -0700

    added comments to MCP3008

--- libraries/MCP3008/MCP3008.cpp
+++ libraries/MCP3008/MCP3008.cpp
@@ -1,34 +1,42 @@
+/*
+This file contains the implementations of the functions inside "MCP3008.h"
+
+Phillip Noel
+27-MAR-2017
+*/
 #include "MCP3008.h"
 
 int setupMCP3008(int newCS){
   if(newCS < 0)
     return IOT_FAILURE;
 
-  pinMode(newCS, OUTPUT);
+  pinMode(newCS, OUTPUT);                                         //if the cs is low when the mcp3008 is powered on then it must be cycled
   digitalWrite(newCS, HIGH);
   digitalWrite(newCS, LOW);
   digitalWrite(newCS, HIGH);
 
-  SPI.begin();
+  SPI.begin();                                                    //always setFrequency() after SPI.begin()
   SPI.setFrequency(1350000);
 
   return IOT_UNKNOWN;
 }
 
-uint16_t readMCP(uint8_t channel, int cs){
-  byte one = 0b00000001;
-  byte two = 0b10000000 | (channel << 4);
-  byte three = 0b00000000;
+uint16_t readMCP3008(uint8_t channel, int cs){
+  byte one   = 0b00000001;                                        //start bit is separate from configuration bit in order to simplify output bits
+  byte two   = 0b10000000 | (channel << 4);                       //differential bit and 3 bits for the channel. Next two bits received are not used, last 2 are
+  byte three = 0b00000000;                                        //these 8 bits are part of the output
+
   byte bytesBack[3];
 
-  digitalWrite(cs, LOW);
 
-                 SPI.transfer(one);
-  bytesBack[1] = SPI.transfer(two);
-  bytesBack[2] = SPI.transfer(three);
+  digitalWrite(cs, LOW);                                          //bring cs low to communicate with device
 
-  digitalWrite(cs, HIGH);
+                 SPI.transfer(one);                               //don't need bytes read back
+  bytesBack[1] = SPI.transfer(two);                               //will only need last 2 bytes of response
+  bytesBack[2] = SPI.transfer(three);                             //need all 8 bytes of response
 
-  return ( ((uint16_t)(bytesBack[1] % 4) << 8) | bytesBack[2] );
+  digitalWrite(cs, HIGH);                                         //bring cs high when done communicating
+
+  return ( ((uint16_t)(bytesBack[1] % 4) << 8) | bytesBack[2] );  //Output of MCP3008 is Most Significant Bit first (MSB). Need last 2 bits of bytesBack[1] and all 8 bits of bytesBack[2]
 
 }
--- libraries/MCP3008/MCP3008.h
+++ libraries/MCP3008/MCP3008.h
@@ -1,3 +1,14 @@
+/*
+This header file contains functions used to controll the MCP3008 8-channel, 10-bit analog to digital converter.
+The setupMCP3008 function sets speed of the SPI hardware
+The read MCP3008 function interfaces with the device.
+This code is not encapsulated in an object for the sake of speed/efficiency. Therefore, since there is no object containing variables to which these function can refer,
+the functions must be supplied a channel and chip select pin for readings.
+
+Phillip Noel
+27-MAR-2017
+*/
+
 #if !defined(MCP3008_H)
 #define MCP3008_H
 
@@ -5,6 +16,7 @@
 #include <SPI.h>
 
 int 	 setupMCP3008(int);
-uint16_t readMCP(uint8_t, int);
+
+uint16_t readMCP3008(uint8_t, int);
 
 #endif
